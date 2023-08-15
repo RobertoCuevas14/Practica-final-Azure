4a963340818caea94b7d29664303cdce0e1414fc
Misaelurb <misaelfup@gmail.com>
2023-08-15 10:22:36 -0400

    cambios

---
+++ test/app.test.js
@@ -0,0 +1,16 @@
+const request = require('supertest');
+const app = require('../app'); // Ajusta la ruta según tu estructura de carpetas
+const expect = require('chai').expect;
+
+describe('GET /', function () {
+  it('Debería devolver el mensaje "¡Hola, mundo!"', function (done) {
+    request(app)
+      .get('/')
+      .expect(200)
+      .end(function (err, res) {
+        if (err) return done(err);
+        expect(res.text).to.equal('¡Hola, mundo!');
+        done();
+      });
+  });
+});
